fd04e0de08961a0acce51829bb4d1a63d8756e7b
Grim Kriegor <grimkriegor@krutt.org>
2019-03-27 05:22:29 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,58 @@
+# Estus Flask lamp
+
+Ambient light that resembles a flickering Estus Flask sitting on a stone base, with multiple modes of operation, namely regular Estus Flask and Ashen Estus Flask.
+
+## Getting
+
+```
+git clone --recursive https://github.com/GrimKriegor/EstusFlask.git
+cd EstusFlask/
+```
+
+## Printing
+
+### Flask
+
+models/flask.blend
+
+```
+Material       | Clear PLA
+Layer height   | 0.1 mm
+Wall thickness | 0.8 mm
+Temperature    | 195° C
+Infill         | Concentric 5%
+Supports       | None
+Base adhesion  | Brim 8 mm
+```
+
+### Base
+
+models/base.fcstd
+
+```
+Material       | Gray PLA
+Layer height   | 0.2 mm
+Wall thickness | 1.2 mm
+Temperature    | 200° C
+Infill         | Grid 15%
+Supports       | Touching buildplate
+Base adhesion  | Skirt
+```
+
+## Wiring
+
+RGB LED connected to pins 9, 10 and 11 an USB powered AVR ATMega328p board through three 220 ohm resistors.
+
+
+
+## Building and uploading the code
+
+```
+cd src/
+make upload
+```
+
+
+## Attribuition
+
+`models/flask.blend` is a fork of [Estus Flask by 3D2N](https://www.thingiverse.com/thing:1543005) licensed under the [Creative Commons - Attribution license](http://creativecommons.org/licenses/by/3.0/).
---
+++ src/flask.cpp
@@ -0,0 +1,105 @@
+#include <Arduino.h>
+#include <LEDController.h>
+
+
+/* LED initialization */
+LEDController red(9),
+              green(10),
+              blue(11);
+
+
+/* Mode specific configuration variables */
+LEDController *main_led,
+              *secondary_led;
+
+uint8_t       main_led_intensity_max,
+              main_led_intensity_min,
+              secondary_led_intensity_max,
+              secondary_led_intensity_min;
+
+int           main_led_duration_min,
+              main_led_duration_max,
+              secondary_led_duration_min,
+              secondary_led_duration_max;
+
+
+/* State variables */
+uint8_t       main_led_intensity,
+              secondary_led_intensity = 0;
+
+
+/*
+ * Initialize configuration variables
+ * according to mode
+ */
+void initMode(uint8_t mode) {
+  switch(mode) {
+
+    /* Estus Flask */
+    default:
+      main_led                    = &red;
+      secondary_led               = &green;
+      main_led_intensity_min      = 255;
+      main_led_intensity_max      = 255;
+      main_led_duration_min       = 2000;
+      main_led_duration_max       = 2000;
+      secondary_led_intensity_min = 0;
+      secondary_led_intensity_max = 64;
+      secondary_led_duration_min  = 100;
+      secondary_led_duration_max  = 1000;
+      break;
+
+    /* Ashen Estus Flask */
+    case 3:
+      main_led                    = &blue;
+      secondary_led               = &green;
+      main_led_intensity_min      = 192;
+      main_led_intensity_max      = 255;
+      main_led_duration_min       = 1000;
+      main_led_duration_max       = 5000;
+      secondary_led_intensity_min = 0;
+      secondary_led_intensity_max = 128;
+      secondary_led_duration_min  = 250;
+      secondary_led_duration_max  = 2000;
+      break;
+
+  }
+}
+
+
+/*
+ * Boot sequence
+ */
+void setup () {
+
+  /* Seed the RNG */
+  randomSeed(analogRead(0));
+
+  /* Get a random mode and initialize it */
+  uint8_t mode = random(0, 4);
+  initMode(mode);
+
+}
+
+
+/*
+ * Main loop
+ */
+void loop() {
+
+  /* Generate cycle specific values for intensity and duration */
+  uint8_t main_led_intensity_new       = random(main_led_intensity_min, main_led_intensity_max+1);
+  int main_led_duration_new            = random(main_led_duration_min, main_led_duration_max+1);
+  uint8_t secondary_led_intensity_new  = random(secondary_led_intensity_min, secondary_led_intensity_max+1);
+  int secondary_led_duration_new       = random(secondary_led_duration_min, secondary_led_duration_max+1);
+
+  /* Change LED intensity accordingly */
+  main_led->stepDim(main_led_duration_new, main_led_intensity, main_led_intensity_new);
+  secondary_led->stepDim(secondary_led_duration_new, secondary_led_intensity, secondary_led_intensity_new);
+
+
+  /* Store this cycle's intensity values in memory for next cycle's transition */
+  main_led_intensity                   = main_led_intensity_new;
+  secondary_led_intensity              = secondary_led_intensity_new;
+
+}
